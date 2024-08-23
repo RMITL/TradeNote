c044174b04d6071162fb5d53cc389001fd158802
Jay Hirschfeld <134090509+RMITL@users.noreply.github.com>
2024-08-23 15:56:31 -0600

    Add files via upload

--- index.mjs
+++ index.mjs
@@ -11,7 +11,7 @@ import { useImportTrades, useGetExistingTradesArray, useUploadTrades } from './s
 import { currentUser, uploadMfePrices, existingTradesArray, tradesData, existingImports } from './src/stores/globals.js';
 import { useGetTimeZone } from './src/utils/utils.js';
 
-const databaseURI = "mongodb+srv://info:WuEQuHxL4xZa62IE@cluster0.be4cj.mongodb.net/Cluster0?retryWrites=true&w=majority&appName=Cluster0";
+const databaseURI = "mongodb+srv://info:WuEQuHxL4xZa62IE@cluster0.be4cj.mongodb.net/Cluster0?retryWrites=true&w=majority";
 
 const client = new MongoClient(databaseURI, {
     serverApi: {
